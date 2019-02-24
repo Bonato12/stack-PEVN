ffbbf4b6263999170430e8a7eddfe0ed1739b789
Sebastian Bonato <sebabonato12@gmail.com>
2019-02-24 11:26:27 -0300

    Termine Cliente Controller Backend

--- server/routes/cliente.js
+++ server/routes/cliente.js
@@ -1,42 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var app = express();
-var pg = require('pg');
 var nodemailer = require('nodemailer');
 const clienteController = require('./controller/cliente');
 
 
-
 router.get('/cliente', clienteController.listaclientes);
 router.post('/cliente',clienteController.addcliente);
+router.get('/cliente/:id_cliente',clienteController.getidcliente);
+router.delete('/cliente/:id_cliente',clienteController.deletecliente);
+router.put('/cliente/:id_cliente',clienteController.updatecliente);
 
 
-
-  router.route('/cliente/:id_cliente').get((req,res)=>{
-       pool.query('SELECT * FROM cliente WHERE id_cliente=($1)', [req.params.id_cliente])
-      .then(response=> {
-        res.json(response.rows);
-      }).catch(error =>{
-        console.log(error);
-      });
-  }).delete((req,res)=> {
-        console.log("Peticion DELETE");
-        pool.query("DELETE FROM cliente WHERE id_cliente=($1)",[req.params.id_cliente]).then(response=> {
-        console.log(response.rows)
-        res.json(response.rows);
-        }).catch(error =>{
-          console.log(error);
-        })
-  }).put((req,res)=>{
-        console.log("Peticion UPDATE");
-        pool.query("UPDATE cliente SET dni=($1), nombre=($2), apellido=($3), ciudad=($4), direccion=($5), telefono=($6), mail=($7) WHERE id_cliente=($8)", [req.body.dni, req.body.nombre, req.body.apellido,req.body.ciudad,req.body.direccion,req.body.telefono,req.body.mail,req.params.id_cliente]).then(response=> {
-        console.log(response.rows)
-        res.json(response.rows);
-        }).catch(error =>{
-          console.log(error);
-        })
-
-  });
 
   router.route('/email').post((req,res)=>{
     console.log(req.body);
