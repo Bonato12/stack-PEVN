d1a6fada57fc7e1a07aadd7c11bc56748d6a4ad3
Sebastian Bonato <sebabonato12@gmail.com>
2019-03-05 08:23:49 -0300

    exporte imgData y arregle editar Producto

--- client/models/Producto.js
+++ client/models/Producto.js
@@ -2,7 +2,7 @@ export default class Producto {
     constructor(id_producto,modelo,marca,descripcion,tipoProducto,stock,precio){
           this.id_producto =   id_producto,
           this.modelo = modelo,
-          this.modelo = marca,
+          this.marca = marca,
           this.descripcion = descripcion,
           this.tipoProducto = tipoProducto,
           this.stock = stock,
