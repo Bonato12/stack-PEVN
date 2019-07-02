cd38b96be7b74ae39a5c7e51009c649198768fb1
Sebastian Bonato <sebabonato12@gmail.com>
2019-07-02 17:03:47 -0300

    Nuevo Estulo en el NavBar

--- client/App.vue
+++ client/App.vue
@@ -7,37 +7,37 @@
                 <div class="navbar-collapse" style="margin-left:150px;">
                     <ul class="navbar-nav mx-auto">
                         <li class="nav-item active">
-                            <router-link   tag="li" active-class="activo" id="cliente"   to="/HomeCliente">
+                            <router-link   tag="a" active-class="activo" id="cliente"   to="/HomeCliente">
                               <i class="fas fa-wheelchair"></i>
                               Clientes
                             </router-link>
                         </li>
                         <li class="nav-item">
-                            <router-link  tag="li" active-class="activo"   to="/HomeProducto">
+                            <router-link  tag="a" active-class="activo"   to="/HomeProducto">
                               <i class="fas fa-mobile-alt"></i>
                               Productos
                             </router-link>
                         </li>
                         <li class="nav-item">
-                            <router-link  tag="li" active-class="activo"   to="/HomeVenta">
+                            <router-link  tag="a" active-class="activo"   to="/HomeVenta">
                               <i class="fas fa-shopping-cart"></i>
                               Ventas
                             </router-link>
                         </li>
                         <li class="nav-item">
-                            <router-link  tag="li" active-class="activo"   to="/HomeCompra">
+                            <router-link  tag="a" active-class="activo"   to="/HomeCompra">
                               <i class="fas fa-coins"></i>
                               Compras
                             </router-link>
                         </li>
                         <li class="nav-item">
-                            <router-link   tag="li"active-class="activo"   to="/HomeArreglo">
+                            <router-link   tag="a"active-class="activo"   to="/HomeArreglo">
                               <i class="fas fa-tools"></i>
                               Arreglos
                             </router-link>
                         </li>
                         <li class="nav-item">
-                            <router-link   tag="li" active-class="activo"   to="/HomeProveedor">
+                            <router-link   tag="a" active-class="activo"   to="/HomeProveedor">
                               <i class="fas fa-people-carry"></i>
                               Proveedores
                             </router-link>
@@ -122,7 +122,7 @@ background-blend-mode: multiply;
 }
 
 .navbar{background:#222222;}
-.nav-item::after{content:'';display:block;width:0px;height:2px;background:#fec400;transition: 0.2s;}
+.nav-item::after{content:'';display:block;width:0px;height:1px;background:#fec400;transition: 0.2s;}
 .nav-item:hover::after{width:100%;}
 .nav-link{padding:15px 5px;transition:0.2s;}
 
@@ -131,51 +131,42 @@ background-blend-mode: multiply;
 .activo{
     background-color: #fec400;
     color: black;
-    height: 193%;
+    height: 183%;
+    width:auto;
+    margin-top:-55;
+
+
+}
+
+a{
+  height: 120%;
+  display: inline-block;
+
+
+}
+
+a:link
+{
+text-decoration:none;
+color:white;
+}
+
+a:hover{
+  color:white;
 }
 
 
-nav li{
+nav a{
     color: white;
+    padding: 4px 1px 4px 1px;
 }
 
 nav{
-     height: 65px;
+     height: 60px;
      text-align:center;
-
-
 }
 
-/* DROPDOWN DEL USUARIO */
-.dropbtn {
-  background-color: rgba(0,0,0,0.7);
-  color: white;
-  font-size: 16px;
-  border: none;
-}
 
-.dropdown {
-  background-color: rgba(0,0,0,0.7);
-  position: relative;
-  display: inline-block;
-}
 
-.dropdown-content {
-  display: none;
-  position: absolute;
-  background-color: white;
-  min-width: 160px;
-}
-
-.dropdown-content a {
-  color: black;
-  padding: 12px 16px;
-  text-decoration: none;
-  display: block;
-}
-
-.dropdown:hover .dropdown-content {
-  display: block;
-}
 
 </style>
