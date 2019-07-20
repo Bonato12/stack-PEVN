e4a484cf609c78d20ccc0c33b27bcf98b2d4fc45
Sebastian Bonato <sebabonato12@gmail.com>
2019-07-20 11:24:20 -0300

    Cree Modelo Usuario

---
+++ client/models/Usuario.js
@@ -0,0 +1,9 @@
+export default class Usuario {
+    constructor(id_usuario,uid,mail,password,rol){
+          this.id_usuario =  id_usuario,
+          this.uid = uid,
+          this.mail = mail,
+          this.password = password,
+          this.rol = rol
+    }
+}
